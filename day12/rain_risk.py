e98a1f8c028b4a4199e191fab677989e7c22fb7e
ryan brenon <rbrenon@gmail.com>
2022-11-25 16:57:36 -0500

    day 12-2

--- day12/rain_risk.py
+++ day12/rain_risk.py
@@ -7,6 +7,12 @@ def main():
 
     print(f"Part one answer is: {part_one(instructions)}")
 
+    starting_waypoint = {
+        'x': 10,
+        'y': 1
+    }
+    print(f"Part two answer is: {part_two(instructions, starting_waypoint)}")
+
 
 def part_one(instructions: list[str], starting_direction: str = 'E') -> int:
     """
@@ -25,7 +31,7 @@ def part_one(instructions: list[str], starting_direction: str = 'E') -> int:
     :return: sum of absolute value from 0,0 to the last ferry location
     """
     direction: str = starting_direction
-    fleet_coord = {
+    ferry_coord = {
         'x': 0,
         'y': 0
     }
@@ -45,21 +51,81 @@ def part_one(instructions: list[str], starting_direction: str = 'E') -> int:
             # turn the ferry left by the number of degrees indicated - eg: L90 steers left 90 degrees
             new_direction = (compass[direction] - int(action[1:])) % 360
             direction = [k for k in compass if compass[k] == new_direction][0]
-
         elif action[0] == 'N' or (action[0] == 'F' and direction == 'N'):
-            # increase value of y axis
-            fleet_coord['y'] += int(action[1:])
+            # increase y axis of ferry
+            ferry_coord['y'] += int(action[1:])
         elif action[0] == 'E' or (action[0] == 'F' and direction == 'E'):
-            # increase value of x axis
-            fleet_coord['x'] += int(action[1:])
+            # increase x axis of ferry
+            ferry_coord['x'] += int(action[1:])
         elif action[0] == 'S' or (action[0] == 'F' and direction == 'S'):
-            # decrease value of y axis
-            fleet_coord['y'] -= int(action[1:])
+            # decrease y axis of ferry
+            ferry_coord['y'] -= int(action[1:])
         elif action[0] == 'W' or (action[0] == 'F' and direction == 'W'):
-            # decrease value of x axis
-            fleet_coord['x'] -= int(action[1:])
+            # decrease x axis of ferry
+            ferry_coord['x'] -= int(action[1:])
 
-    manhattan_distance: int = abs(fleet_coord['x']) + abs(fleet_coord['y'])     # 364
+    # calculate distance to 0,0
+    manhattan_distance: int = abs(ferry_coord['x']) + abs(ferry_coord['y'])     # 364
+
+    return manhattan_distance
+
+
+def part_two(instructions: list[str], waypoint: dict[str:int], starting_direction: str = 'E') -> int:
+    """
+    Almost all the actions indicate how to move a waypoint which is relative to the ship's position:
+
+        Action N means to move the waypoint north by the given value.
+        Action S means to move the waypoint south by the given value.
+        Action E means to move the waypoint east by the given value.
+        Action W means to move the waypoint west by the given value.
+        Action L means to rotate the waypoint around the ship left (counter-clockwise) the given number of degrees.
+        Action R means to rotate the waypoint around the ship right (clockwise) the given number of degrees.
+        Action F means to move forward to the waypoint a number of times equal to the given value.
+        The waypoint starts 10 units east and 1 unit north relative to the ship. The waypoint is relative to the ship;
+        if the ship moves, the waypoint moves with it.
+
+    :param starting_direction:
+    :param instructions:
+    :param waypoint:
+    :return:
+    """
+    direction: str = starting_direction
+    ferry_coord = {
+        'x': 0,
+        'y': 0
+    }
+
+    for action in instructions:
+        if action[0] == 'F':
+            # move to the waypoint x times
+            ferry_coord['x'] += (waypoint['x'] * int(action[1:]))
+            ferry_coord['y'] += (waypoint['y'] * int(action[1:]))
+
+        elif action[0] == 'R':
+            # waypoint (2,1) rotated 90 = (1,-2)
+            # x,y = -y,x for each 90 clockwise rotation
+            rotations = int(action[1:]) // 90
+            for rotation in range(rotations):   # apply 1x 90-degree clockwise rotation
+                waypoint['x'], waypoint['y'] = waypoint['y'] , (waypoint['x'] * -1)
+        elif action[0] == 'L':
+            # x,y = y,-x for each 90 counter-clockwise rotation
+            rotations = int(action[1:]) // 90
+            for rotation in range(rotations):   # apply 1x 90-degree counter-clockwise rotation
+                waypoint['x'], waypoint['y'] = (waypoint['y'] * -1), waypoint['x']
+        elif action[0] == 'N':
+            # increase y axis value of waypoint
+            waypoint['y'] += int(action[1:])
+        elif action[0] == 'E':
+            # increase x axis value of waypoint
+            waypoint['x'] += int(action[1:])
+        elif action[0] == 'S':
+            # decrease y axis value of waypoint
+            waypoint['y'] -= int(action[1:])
+        elif action[0] == 'W':
+            # decrease x axis value of waypoint
+            waypoint['x'] -= int(action[1:])
+
+    manhattan_distance: int = abs(ferry_coord['x']) + abs(ferry_coord['y'])  # 39518, too low: 1991, 37792
 
     return manhattan_distance
 
